06a55f83169360f57989a794e452a238dba9f4ee
Wera Stremedlowska <stremedlowska@gmail.com>
2024-04-15 18:39:42 +0200

    Added MagentoApiTest (#3)

---
+++ src/test/java/models/ExtensionAttributes.java
@@ -0,0 +1,12 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ExtensionAttributes {
+    @JsonProperty("is_subscribed")
+    private boolean isSubscribed;
+
+    public boolean isSubscribed() {
+        return false;
+    }
+}
