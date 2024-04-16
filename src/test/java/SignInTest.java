d0416a69ab4e49871d6d86ada3cbe0e510c967c5
ViktoriiaKarpus <vikakarpys@gmail.com>
2024-04-16 04:39:15 +0200

    Refactor

--- src/test/java/SignInTest.java
+++ src/test/java/SignInTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 
 public class SignInTest extends BaseTest {
@@ -12,6 +12,10 @@ public class SignInTest extends BaseTest {
     @Test
     public void testSingInPW() {
         getPage().navigate("https://magento.softwaretestingboard.com/");
+
+        getPage().getByLabel("Diese Website bittet um die").locator("div")
+                .filter(new Locator.FilterOptions().setHasText("Herzlich willkommenDiese")).nth(1).click();
+        getPage().getByLabel("Einwilligen", new Page.GetByLabelOptions().setExact(true)).click();
 
         getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("\n" +
                 "Sign In ")).click();
@@ -22,10 +26,7 @@ public class SignInTest extends BaseTest {
         getPage().getByLabel("Password").fill("123456789vk_");
         getPage().getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign In")).click();
 
-        String text = getPage().locator("body>div.page-wrapper>header>div.panel.wrapper>div>ul")
-                .innerText().substring(0,25);
-
-        Assert.assertEquals(text, "Welcome, tester3 tester3!");
+        assertThat(getPage().getByRole(AriaRole.BANNER).getByText("Welcome, tester3 tester3!")).isVisible();
 
     }
 }
