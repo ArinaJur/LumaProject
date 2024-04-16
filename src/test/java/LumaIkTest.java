1fe00f1e45f1ad14fa765b087a616b18fdb97399
IrynaKolyada <iryna.kolyada.us@gmail.com>
2024-04-15 18:03:01 -0600

    add LumaIkTest (#5)

---
+++ src/test/java/LumaIkTest.java
@@ -0,0 +1,39 @@
+import com.microsoft.playwright.Locator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class LumaIkTest extends BaseTest {
+
+    @Test
+    public void testNavigationBar(){
+        List<String> expectedListOfNavBar = List.of("What's New", "Women", "Men", "Gear", "Training","Sale");
+
+        getDriver().get("https://magento.softwaretestingboard.com/");
+        List<WebElement> listOfNavBar = getDriver().findElements(By.cssSelector(".nav-sections .navigation >ul>li>a"));
+        List<String> namesOfNavbar = new ArrayList<>();
+        for(WebElement text : listOfNavBar) {
+            namesOfNavbar.add(text.getText());
+        }
+
+        Assert.assertEquals(namesOfNavbar, expectedListOfNavBar);
+    }
+
+    @Test
+    public void testNavigationBarPW(){
+        String [] expectedListOfNavBar = {"What's New", "Women", "Men", "Gear", "Training", " Sale"} ;
+
+        getPage().navigate("https://magento.softwaretestingboard.com/");
+        Locator navBar = getPage().locator(".nav-sections .navigation >ul>li>a");
+
+        assertThat(navBar).hasCount(6);
+        assertThat(navBar).containsText(expectedListOfNavBar);
+    }
+
+}
