776d55d2dcd90cf7cf2c791b7570427f8e2fe68c
ViktoriiaKarpus <vikakarpys@gmail.com>
2024-04-16 04:21:17 +0200

    Refactor

--- src/test/java/SignInTest.java
+++ src/test/java/SignInTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import runner.BaseTest;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-import static org.testng.AssertJUnit.assertEquals;
+
 
 public class SignInTest extends BaseTest {
 
@@ -12,15 +12,20 @@ public class SignInTest extends BaseTest {
     public void testSingInPW() {
         getPage().navigate("https://magento.softwaretestingboard.com/");
 
-            getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("\n" +
-                    "Sign In ")).click();
-            getPage().getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).click();
-            getPage().getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).fill("Tester3@gmail.com");
-            getPage().getByLabel("Password").click();
-            getPage().getByLabel("Password").fill("123456789vk_");
-            getPage().getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign In")).click();
+        getPage().getByLabel("Diese Website bittet um die").locator("div")
+                .filter(new Locator.FilterOptions().setHasText("Herzlich willkommenDiese")).nth(1).click();
+        getPage().getByLabel("Einwilligen", new Page.GetByLabelOptions().setExact(true)).click();
 
-            assertThat(getPage().getByRole(AriaRole.BANNER).getByText("Welcome, tester3 tester3!")).isVisible();
+        getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("\n" +
+                "Sign In ")).click();
+
+        getPage().getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).click();
+        getPage().getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).fill("Tester3@gmail.com");
+        getPage().getByLabel("Password").click();
+        getPage().getByLabel("Password").fill("123456789vk_");
+        getPage().getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign In")).click();
+
+        assertThat(getPage().getByRole(AriaRole.BANNER).getByText("Welcome, tester3 tester3!")).isVisible();
 
     }
 }
