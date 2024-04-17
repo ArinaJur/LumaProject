8c640b2e4cba5284700f0909a566290c09923d43
IrynaKolyada <iryna.kolyada.us@gmail.com>
2024-04-17 16:40:31 -0600

    add testSingIn

--- src/test/java/LumaIkTest.java
+++ src/test/java/LumaIkTest.java
@@ -1,4 +1,6 @@
 import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -34,6 +36,23 @@ public class LumaIkTest extends BaseTest {
 
         assertThat(navBar).hasCount(6);
         assertThat(navBar).containsText(expectedListOfNavBar);
+    }
+
+    @Test
+    public void testSingInPW() {
+        getPage().navigate("https://magento.softwaretestingboard.com/");
+
+
+        getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("\n" +
+                "Sign In ")).click();
+        getPage().getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).click();
+        getPage().getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).fill("tester1234@gmail.com");
+        getPage().getByLabel("Password").click();
+        getPage().getByLabel("Password").fill("tester1234!");
+        getPage().getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign In")).click();
+        String text = getPage().locator("body > div.page-wrapper > header > div.panel.wrapper > div > ul").innerText().substring(0,23);
+
+        Assert.assertEquals(text, "Welcome, Tester Tester!");
     }
 
 }
