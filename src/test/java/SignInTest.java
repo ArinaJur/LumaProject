8d161a5b55ffb294b7753eb049d634f15675c65f
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2024-04-17 21:28:43 -0400

    Try CI run

--- src/test/java/SignInTest.java
+++ src/test/java/SignInTest.java
@@ -7,84 +7,36 @@ import runner.BaseTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class SignInTest extends BaseTest {
     private static final Logger logger = LoggerFactory.getLogger(SignInTest.class);
 
     @Test
     public void testSingInPW() throws InterruptedException {
-        logger.info("Navigating to the website");
         getPage().navigate("https://magento.softwaretestingboard.com/");
 
-        String consentButtonSelector = "body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button > p";
-        logger.info("Checking for visibility of consent button");
+        Thread.sleep(5000);
+        logger.info(getPage().url());
 
-        if (getPage().isVisible(consentButtonSelector)) {
-            handleOverlays(getPage());
-            getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("\n" +
-                    "Sign In ")).click();
-            logger.info("Consent overlay handled and 'Sign In' clicked");
-        } else {
-            getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("\n" +
-                    "Sign In ")).click();
-            logger.info("'Sign In' clicked without overlay");
-        }
+        getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Sign In ")).click();
 
-        getPage().getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).click();
+        Thread.sleep(5000);
+        logger.info(getPage().url());
+
         getPage().getByLabel("Email", new Page.GetByLabelOptions().setExact(true)).fill("Tester3@gmail.com");
-        getPage().getByLabel("Password").click();
         getPage().getByLabel("Password").fill("123456789vk_");
         getPage().getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign In")).click();
-        getPage().locator(" span.not-logged-in")
-                .getByText("Click “Write for us” link in the footer to submit a guest post").isVisible();
-        logger.info("Form submitted with provided credentials");
 
-        logPageInfo(getPage());
+        Thread.sleep(5000);
+        logger.info(getPage().url());
 
-/*        Thread.sleep(3000);
-        Locator welcomeElement = getPage().locator("//div[@class='panel header']//ul[@class='header links']//li[@class='greet welcome']//span[@class='logged-in']");
-        System.out.println(welcomeElement);
+        Locator welcomeElement = getPage().locator(".panel.header .logged-in");
         String actual = welcomeElement.innerText();
+
+        logger.info(actual);
+
         Assert.assertEquals(actual, "Welcome, tester3 tester3!");
-        Assert.assertTrue(welcomeElement.isVisible());*/
-
-        Locator titleElement = getPage().locator("h1");
-        boolean isVisible = titleElement.isVisible();
-
-        if (isVisible) {
-            logger.info("First element is visible");
-            String titleText = titleElement.innerText();
-            Assert.assertEquals(titleText, "Home Page");
-        } else {
-            Locator secondLocator = getPage().locator(".logged-in").first().getByText("Welcome, tester3 tester3!");
-            boolean isSecondVisible = secondLocator.isVisible();
-            if (isSecondVisible) {
-                logger.info("Second element is visible");
-                String secondText = secondLocator.innerText();
-                Assert.assertEquals(secondText, "Welcome, tester3 tester3!");
-            } else {
-                String welcome = "Welcome, tester3 tester3!";
-                String notVisible = "Element is not visible";
-                String getText = getPage().getByText("Welcome, tester3 tester3!").first().isVisible() ? welcome : notVisible;
-                Assert.assertEquals(getText, "Welcome, tester3 tester3!");
-            }
-        }
+        Assert.assertTrue(welcomeElement.isVisible());
     }
 
-    private void handleOverlays(Page page) {
-        String consentButtonSelector = "body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button > p";
-        page.waitForSelector(consentButtonSelector);
-
-        if (!page.isVisible(consentButtonSelector)) {
-            return;
-        }
-        page.click(consentButtonSelector);
-    }
-
-    private void logPageInfo(Page page) {
-        logger.info("URL: " + page.url());
-        logger.info("Title: " + page.title());
-        logger.info("Content: " + page.content());
-    }
 }
 
