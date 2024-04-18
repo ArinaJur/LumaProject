9a2640799624e3202d32602db63fcfb4dca141ed
ViktoriiaKarpus <vikakarpys@gmail.com>
2024-04-19 00:55:20 +0200

    Ignor LoginPageTest

--- src/test/java/com/rover/model/LoginPageTest.java
+++ src/test/java/com/rover/model/LoginPageTest.java
@@ -1,11 +1,13 @@
 package com.rover.model;
 
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class LoginPageTest extends BaseTest {
 
+    @Ignore
     @Test
     public void testLogin() {
         LoginPage loginPage = new LoginPage(getDriver());
