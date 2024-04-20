220f9ea180a9147c98cc1a4f65fe1d690f9ab409
Pasha <78595276+Lokankara@users.noreply.github.com>
2024-04-20 12:50:38 +0300

    add Playwright test navigator, signup (#16)

---
+++ src/main/java/com/playwright/HomePage.java
@@ -0,0 +1,10 @@
+package com.playwright;
+
+import com.microsoft.playwright.Page;
+
+public class HomePage {
+    private final Page page;
+    public HomePage(Page page) {
+        this.page = page;
+    }
+}
